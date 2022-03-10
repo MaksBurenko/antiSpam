9650d6860e351ad536b0ab94cff9f516eea39a09
Maks Burenko <Cherniymaks@yandex.ru>
2022-03-10 15:08:34 +0300

    123

--- src/antiSpam/Main.java
+++ src/antiSpam/Main.java
@@ -5,17 +5,33 @@ public class Main {
     public static void main(String[] args) {
     }
 
-    interface TextAnalyzer {
-        Label processText(String text);
-    }
     public Label checkLabels(TextAnalyzer[] analyzers, String text) {
         return Label.OK;
     }
 
-    public class SpamAnalyzer implements TextAnalyzer {
+    interface TextAnalyzer {
+        Label processText(String text);
     }
 
-    public class NegativeTextAnalyzer implements TextAnalyzer {
+    public abstract static class KeywordAnalyzer {
+        abstract String getKeywords();
+        Label getLabel(Label Label){
+            return Label;
+        };
+    }
+
+    public class NegativeTextAnalyzer extends KeywordAnalyzer implements TextAnalyzer {
+    }
+
+    public abstract class SpamAnalyzer extends KeywordAnalyzer implements TextAnalyzer {
+        public Label processText(String text){
+            int i = text.indexOf(getKeywords());
+            if (i==-1){
+                return Label.OK;
+            } else{
+                return Label.NEGATIVE_TEXT;
+            }
+        };
     }
 
     public class TooLongTextAnalyzer implements TextAnalyzer {

7dd0d9eb2a33170af1945f86fa70c72b2ecb2f2b
Maks Burenko <Cherniymaks@yandex.ru>
2022-03-12 14:18:31 +0300

    123

--- src/antiSpam/Main.java
+++ src/antiSpam/Main.java
@@ -6,35 +6,80 @@ public class Main {
     }
 
     public Label checkLabels(TextAnalyzer[] analyzers, String text) {
-        return Label.OK;
+        TextAnalyzer result;
+        for(int i=0; i < analyzers.length; i++) {
+            if(analyzers[i].equals(Label.OK)) {
+                if(i==analyzers.length-1){
+                    return Label.OK;
+                    break;
+                }
+            } else {
+                return analyzers[i];
+                break;
+            }
+        }
     }
 
     interface TextAnalyzer {
         Label processText(String text);
     }
 
-    public abstract static class KeywordAnalyzer {
-        abstract String getKeywords();
-        Label getLabel(Label Label){
+    abstract static class KeywordAnalyzer implements TextAnalyzer {
+
+        public final String keywords;
+        private final Label Label;
+
+        public KeywordAnalyzer(String keywords, Label Label) {
+            this.keywords = keywords;
+            this.Label = Label;
+        }
+        public String getKeywords() {
+            return keywords;
+        }
+        public Label getLabel() {
             return Label;
-        };
+        }
+
     }
 
-    public class NegativeTextAnalyzer extends KeywordAnalyzer implements TextAnalyzer {
-    }
-
-    public abstract class SpamAnalyzer extends KeywordAnalyzer implements TextAnalyzer {
-        public Label processText(String text){
-            int i = text.indexOf(getKeywords());
-            if (i==-1){
-                return Label.OK;
-            } else{
-                return Label.NEGATIVE_TEXT;
+    static class NegativeTextAnalyzer extends KeywordAnalyzer {
+        NegativeTextAnalyzer(String keywords, Main.Label Label) {
+            super(keywords, Label);
+        }
+        public Label processText(String text) {
+            String negative1 = ":(";
+            String negative2 = "=(";
+            String negative3 = ":|";
+            if ((text.contains(negative1)) || (text.contains(negative2)) || (text.contains(negative3))) {
+                return Main.Label.NEGATIVE_TEXT;
+            }else{
+                return Main.Label.OK;
             }
         };
     }
 
-    public class TooLongTextAnalyzer implements TextAnalyzer {
+    static class SpamAnalyzer extends KeywordAnalyzer {
+        SpamAnalyzer(String keywords, Main.Label Label) {
+            super(keywords, Label);
+        }
+        public Label processText(String text) {
+            if (text.contains(keywords)) {
+                return Main.Label.SPAM;
+            }else{
+                return Main.Label.OK;
+            }
+        };
+    }
+
+    static class TooLongTextAnalyzer implements TextAnalyzer {
+        int maxLength;
+        public Label processText(String text) {
+            if (text.length() > maxLength) {
+                return Main.Label.TOO_LONG;
+            }else{
+                return Main.Label.OK;
+            }
+        };
     }
 
     enum Label {
